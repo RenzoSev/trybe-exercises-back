e8da0343083713c5e54517e1ff40c4b8e9af0753
RenzoSev <sevilharenzo@gmail.com>
2021-08-10 15:18:19 -0300

    feat: prize

--- Bloco 26 - Introdução ao desenvolvimento Web com NodeJS/26.1/imc/prize.ts
+++ Bloco 26 - Introdução ao desenvolvimento Web com NodeJS/26.1/imc/prize.ts
@@ -1,1 +1,30 @@
 const readline = require('readline-sync');
+
+export const getRandomNumber = () => Math.round(Math.random() * 10);
+
+export const questionNumberChoice = () => readline.questionInt('Digite um número');
+
+export const getThePrize = (choice: number) => {
+  const randomNumber = getRandomNumber();
+
+  const isRightChoice = randomNumber === choice;
+
+  if (isRightChoice)
+    return `Parabéns! Você acertou o número.
+  Sua resposta: ${choice}.
+  Número escolhido: ${randomNumber}`;
+
+  return `Você não acertou o número =(.
+  Sua resposta: ${choice}.
+  Número escolhido: ${randomNumber}`;
+};
+
+export const runPrize = () => {
+  const choice = questionNumberChoice();
+
+  const prizeResult = getThePrize(choice);
+
+  console.log(prizeResult);
+}
+
+runPrize();
