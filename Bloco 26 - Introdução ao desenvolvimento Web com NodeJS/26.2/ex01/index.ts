96ff5d8d80dbfb18fddcb9179b3696ae23c80c3d
RenzoSev <sevilharenzo@gmail.com>
2021-08-11 14:46:16 -0300

    feat: ex02 03

--- Bloco 26 - Introdução ao desenvolvimento Web com NodeJS/26.2/ex01/index.ts
+++ Bloco 26 - Introdução ao desenvolvimento Web com NodeJS/26.2/ex01/index.ts
@@ -1,3 +1,11 @@
+const getRandomNumber = () => Math.round(Math.random() * 100);
+
+const getThreeRNS = () => ({
+  firstRN: getRandomNumber(),
+  secondRN: getRandomNumber(),
+  thirdRN: getRandomNumber(),
+});
+
 const numPromise = (a: number, b: number, c: number) => {
   const promise = new Promise((resolve, reject) => {
     if (isNaN(a) || isNaN(b) || isNaN(c)) reject(Error('Não é um número'));
@@ -9,3 +17,25 @@ const numPromise = (a: number, b: number, c: number) => {
 
   return promise;
 };
+
+const useThenCatchToGetPromise = () => {
+  const { firstRN, secondRN, thirdRN } = getThreeRNS();
+
+  numPromise(firstRN, secondRN, thirdRN)
+    .then((result) => console.log(result))
+    .catch((err) => console.error(err));
+};
+
+const useAsyncAwaitToGetPromise = async () => {
+  const { firstRN, secondRN, thirdRN } = getThreeRNS();
+
+  try {
+    const result = await numPromise(firstRN, secondRN, thirdRN);
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+useThenCatchToGetPromise();
+useAsyncAwaitToGetPromise();
