f308bf58b644cdef55c3073123496e80e93f6ca1
RenzoSev <sevilharenzo@gmail.com>
2021-08-14 17:05:07 -0300

    feat: ex 02

--- Bloco 26 - Introdução ao desenvolvimento Web com NodeJS/26.4/ex/index.ts
+++ Bloco 26 - Introdução ao desenvolvimento Web com NodeJS/26.4/ex/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser());
 
 app.get('/', (req, res) => {
   res.status(200).send('Home Page');
@@ -10,6 +13,14 @@ app.get('/ping', (req, res) => {
   const pong = { message: 'pong' };
 
   res.json(pong);
+});
+
+app.post('/hello', (req, res) => {
+  const name = req.body.name as string;
+
+  const message = { message: `Hello, ${name}` };
+
+  res.json(message);
 });
 
 app.listen(3000, () => console.log('Server is running! '));
