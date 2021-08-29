e1622ee0d63fdd5b9d2603e4ab8d073638cfac5b
RenzoSev <sevilharenzo@gmail.com>
2021-08-29 18:43:59 -0300

    feat: png filter

--- Bloco 28 - Autenticação e Upload de Arquivos/28.2/index.js
+++ Bloco 28 - Autenticação e Upload de Arquivos/28.2/index.js
@@ -4,22 +4,42 @@ const multer = require('multer');
 const app = express();
 
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
+  destination: (_req, _file, callback) => {
     callback(null, 'uploads');
   },
-  filename: (req, file, callback) => {
+  filename: (_req, file, callback) => {
     callback(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype !== 'image/png') {
+    req.fileValidationError = true;
+
+    return cb(null, false);
+  }
+
+  return cb(null, true);
+};
+
+const upload = multer({ storage, fileFilter });
 
 app.use(express.json());
 
 app.use(express.static(__dirname + '/uploads'));
 
 app.post('/upload', upload.single('file'), (req, res) => {
-  res.status(200).send(req.file);
+  if (req.fileValidationError) {
+    const err = {
+      error: {
+        message: 'Extension must be png',
+      },
+    };
+
+    return res.status(403).json(err);
+  }
+
+  return res.status(200).send(req.file);
 });
 
 app.listen(3000, () => console.log('Server is running!'));
